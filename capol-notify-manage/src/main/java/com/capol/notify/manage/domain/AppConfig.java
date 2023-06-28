fec5f35a72977659155021bff8bcaaef32d8910d
heyong_1988@126.com <heyong1988gitee>
2023-06-28 20:11:23 +0800

    声明线程池参数配置方式调整,通过获取当前活跃CPU数量配置。

--- capol-notify-manage/src/main/java/com/capol/notify/manage/domain/AppConfig.java
+++ capol-notify-manage/src/main/java/com/capol/notify/manage/domain/AppConfig.java
@@ -13,6 +13,11 @@ import java.util.concurrent.Executor;
 @EnableAsync
 public class AppConfig {
     /**
+     * 获取当前活跃CPU数量
+     */
+    private static int NUMBER_OF_CORES = Math.max(4, Math.min(Runtime.getRuntime().availableProcessors(), 16));
+
+    /**
      * 声明一个线程池，用于执行异步任务
      *
      * @return
@@ -20,9 +25,16 @@ public class AppConfig {
     @Bean("Capol-Notify-ThreadPool")
     public Executor myThreadPool() {
         ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
-        executor.setCorePoolSize(10);
-        executor.setMaxPoolSize(20);
-        executor.setQueueCapacity(50);
+        log.info("-->声明线程池, 当前活跃的CPU数量:{}, CorePoolSize:{}, MaxPoolSize:{}", NUMBER_OF_CORES, NUMBER_OF_CORES + 2, NUMBER_OF_CORES * 2);
+        //核心线程池大小, 线程池中最少会保留的线程数量, 配置参数必须小于MaxPoolSize
+        executor.setCorePoolSize(NUMBER_OF_CORES + 2);
+        //线程池中最大的线程数, 当任务量达到一定程度时, 线程池中的线程数量会动态增加, 直到达到MaxPoolSize后, 这些任务会被放置在任务队列中等待执行
+        executor.setMaxPoolSize(NUMBER_OF_CORES * 2);
+        //任务队列容量, 当任务队列已满时, 新的任务将会被拒绝。
+        executor.setQueueCapacity(25);
+        //空闲线程存活时间
+        executor.setKeepAliveSeconds(5);
+        //线程名称前缘
         executor.setThreadNamePrefix("Capol-Notify-ThreadPool-");
         executor.initialize();
         log.info("-->声明一个线程池，用于执行异步任务!!!");
